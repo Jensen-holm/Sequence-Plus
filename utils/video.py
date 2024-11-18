995b9150777a8bec03b9076737917108dc3e7f88
Jensen Holm <holmj@mail.gvsu.edu>
2024-11-18 14:43:11 -0500

    video utils function now works for getting specific plays

--- utils/video.py
+++ utils/video.py
@@ -1,6 +1,6 @@
 from concurrent.futures import ThreadPoolExecutor, as_completed
-from typing import Optional, Any
-from bs4 import BeautifulSoup
+from typing import Optional, Any, Union
+from bs4 import BeautifulSoup, Tag
 from tqdm import tqdm
 import polars as pl
 import requests
@@ -21,24 +21,37 @@ VIDEO_DIR = os.path.join(
 ) 
 
 
-def get_savant_video(game_pk: int, sv_ids: Optional[list[str]] = None) -> None:
-    game_info = _get_game_info(game_pk=game_pk)
-    assert isinstance(game_info, dict)
+def get_savant_video(plays: pl.DataFrame) -> None:
+    game_infos = {
+        game_pk: _get_game_info(game_pk=game_pk)
+        for game_pk in plays["game_pk"].unique().to_list()
+    }
 
-    play_id_df = _get_play_ids(game_info=game_info, game_pk=game_pk)
-    if sv_ids is not None:
-        play_id_df = (play_id_df
-                      .filter(pl.col("sv_id").is_in(sv_ids)))
+    play_df = pl.concat([
+        _get_play_data(game_info=game_info, game_pk=game_pk)
+        for game_pk, game_info in game_infos.items()
+    ], how="diagonal_relaxed")
+
+    play_df = play_df.join(
+        other=plays.with_columns(
+            pl.col("inning").cast(pl.Int64),
+        ),
+        left_on=["ab_number", "inning", "game_pk", "pitch_number"],
+        right_on=["at_bat_number", "inning", "game_pk", "pitch_number"],
+        how="inner",
+    )
     
+    assert len(play_df["play_id"]) > 0
+
     cpus = os.cpu_count()
     assert cpus is not None
     
-    url_max_workers = min(16, len(play_id_df["play_id"]), cpus * 4)
-    pbar = tqdm(total=len(play_id_df["play_id"]) * 2)
+    url_max_workers = min(16, len(play_df["play_id"]), cpus * 4)
+    pbar = tqdm(total=len(play_df["play_id"]) * 2)
     with ThreadPoolExecutor(max_workers=url_max_workers) as executor:
         video_urls = [
             executor.submit(_get_video_src, play_id)
-            for play_id in play_id_df["play_id"]
+            for play_id in play_df["play_id"]
         ]
 
         results = []
@@ -47,26 +60,31 @@ def get_savant_video(game_pk: int, sv_ids: Optional[list[str]] = None) -> None:
             results.append(video_src)
             pbar.update(1)
         
-        play_id_df = play_id_df.with_columns(
+        play_df = play_df.with_columns(
             pl.Series(
                 name="video_url",
                 values=results,
             )
         )
 
-    play_id_df = play_id_df.drop_nulls(subset="video_url")
-    if play_id_df.is_empty():
+    play_df = play_df.drop_nulls(subset="video_url")
+    if play_df .is_empty():
         return
+    
+    if not os.path.exists(VIDEO_DIR):
+        os.makedirs(VIDEO_DIR)
 
-    video_max_workers = min(16, len(play_id_df["video_url"]), cpus * 4)
+    video_max_workers = min(16, len(play_df["video_url"]), cpus * 4)
     with ThreadPoolExecutor(max_workers=video_max_workers) as executor:
-        pids = [executor.submit(_download_video, v, f"{game_pk}_{i}.mp4")
-                for i, v in enumerate(play_id_df["video_url"])]
+        # pids = [executor.submit(_download_video, v, f"{i}.mp4")
+        #         for i, v in enumerate(play_df["video_url"])]
+        pids = [
+            executor.submit(_download_video, v, f"{game_pk}_{play_id}.mp4")
+            for v, game_pk, play_id in zip(play_df["video_url"], play_df["game_pk"], play_df["play_id"])
+        ]
         for future in as_completed(pids):
             future.result()
             pbar.update(1)
-    
-    pbar.close()
 
 
 def _download_video(video_url: str, save_path: str, _max_retries: int = 5, _wait_time: float = 1.0) -> None:
@@ -74,8 +92,6 @@ def _download_video(video_url: str, save_path: str, _max_retries: int = 5, _wait
         return None
     if not video_url or video_url is None:
         return None
-    if not os.path.exists(VIDEO_DIR):
-        os.makedirs(VIDEO_DIR)
 
     try:
         with requests.get(video_url, stream=True) as r:
@@ -90,7 +106,7 @@ def _download_video(video_url: str, save_path: str, _max_retries: int = 5, _wait
 
 def _get_video_src(
     play_id, _max_retries: int = 5, _wait_time: float = 1.0
-) -> Optional[Any]:
+) -> Optional[str]:
     if _max_retries <= 0:
         return None
 
@@ -101,40 +117,45 @@ def _get_video_src(
 
         soup = BeautifulSoup(resp.content, "html.parser")
         video_container = soup.find("div", attrs={"class": "video-box"})
-        assert (
-            video_container is not None
-        ), f"div w/ class=video-box not found in video container soup"
+        if not isinstance(video_container, Tag):
+            raise ValueError("Video container not found")
 
         video = video_container.find("video")
-        assert (
-            video is not None
-        ), f"video attribute unable to be located in video container soup"
+        assert isinstance(video, Tag)
 
-        src = video.find("source", type="video/mp4")
-        assert src is not None, f"video source could not be found in video soup"
+        source = video.find("source", attrs={"type": "video/mp4"})
+        assert isinstance(source, Tag)
 
-        video_src = src.get("src", None)
-        assert video_src is not None, f"video src could not be found"
+        video_src = source.get("src")
+        assert video_src
+            
+        # Handle the case where video_src could be a string or list of strings
+        if isinstance(video_src, list):
+            return video_src[0] if video_src else None
         return video_src if video_src != "" else None
     except Exception as e:
         print(f"Error: '{e}' \nOccoured when requesting game data from '{video_url}'")
         time.sleep(_wait_time)
-        _get_video_src(
+        return _get_video_src(
             play_id=play_id, _max_retries=_max_retries-1, _wait_time=_wait_time
         )
 
 
-def _get_play_ids(game_info: dict, game_pk: int) -> pl.DataFrame:
-    batters = game_info.get("away_batters", None)
-    assert batters is not None, f"Batter data unavailable for '{game_pk}'"
+def _get_play_data(game_info: dict, game_pk: int) -> pl.DataFrame:
+    away_batters = game_info.get("away_batters", None)
+    assert away_batters is not None, f"Away Batter data unavailable for '{game_pk}'"
+    home_batters = game_info.get("home_batters", None)
+    assert away_batters is not None, f"Home Batter data unavailable for '{game_pk}'"
+
+    batters = away_batters | home_batters
     return pl.concat([
-        pl.from_dict({
-            "sv_id": play.get("sv_id", None),
-            "play_id": play.get("play_id", None),
-        })
+        pl.json_normalize(play, strict=False)
         for player in batters.values()
         for play in player
-    ])
+    ], how="diagonal_relaxed").with_columns(
+        pl.col("game_pk").cast(pl.Int64),
+        pl.col("inning").cast(pl.Int64),
+    )
 
 
 def _get_game_info(
@@ -153,10 +174,18 @@ def _get_game_info(
     except Exception as e:
         print(f"Error: '{e}' \nOccoured when requesting game data from '{game_url}'")
         time.sleep(_wait_time)
-        _get_game_info(
+        return _get_game_info(
             game_pk=game_pk, _max_retries=_max_retries - 1, _wait_time=_wait_time
         )
 
 
 if __name__ == "__main__":
-    _ = get_savant_video(775302)
+    # last and second to last pitches of 2024 WS
+    play_df = pl.from_dict({
+        "game_pk": [775296, 775296],
+        "inning": [9, 9],
+        "at_bat_number": [89, 89],
+        "pitch_number": [4, 3],
+    })
+
+    _ = get_savant_video(plays=play_df)
